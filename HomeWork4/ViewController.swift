2e148e617caa8df4c7b6b54b86109ed9fe762899
LaptevaKate <ekaterina213@yandex.ru>
2022-02-14 20:00:23 +0300

    Initial Commit

---
+++ HomeWork4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork4
+//
+//  Created by Екатерина Лаптева on 14.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
