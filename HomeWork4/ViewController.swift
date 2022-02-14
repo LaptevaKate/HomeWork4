7b3aff8ba26c7b252599bd45d478a13f71b404b7
LaptevaKate <ekaterina213@yandex.ru>
2022-02-14 20:47:36 +0300

    add code

--- HomeWork4/ViewController.swift
+++ HomeWork4/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
    
+        let x = [(1, "x"), (4, "y"), (6, "a")]
+
+        let arrayPow = x.map { element in
+            (element.0 * element.0, element.1)
+        }
+        print(arrayPow)
+
+        let arrayFilter = arrayPow.filter { element in
+            element.0.isMultiple(of: 2)
+        }
+        print(arrayFilter)
+             
+        let arraySorted = arrayFilter.sorted { left, right in
+            return left.1 < right.1
+        }
+        print(arraySorted)
+        
     }
-
-
 }
 
